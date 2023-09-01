178e9e1cc9fd82d88467a450a97deadc33f18911
LXsasse <alexander.sasse@gmail.com>
2023-09-01 12:23:38 -0700

    Add files via upload

--- enformer_analysis/replace_genename.py
+++ enformer_analysis/replace_genename.py
@@ -1,19 +1,23 @@
-# Replace gene names with gene IDs
-
 import numpy as np
 import sys, os
 
-a = np.genfromtxt('Full_analysis/gene-ids-and-positions.tsv', dtype = str, delimiter = '\t', skip_header = 1)
 
+a = np.genfromtxt('gene-ids-and-positions.tsv', dtype = str, delimiter = '\t', skip_header = 1)
 repfile = open(sys.argv[1],'r').readlines()
-obj = open(os.path.splitext(os.path.split(sys.argv[1])[1])[0]+'ENSG.tsv', 'w')
+header = repfile[0].strip().split('\t')
+print(header)
 names = list(a[:,0])
-for h, head in enumerate(repfile):
-    oldname = head.split()[0]
-    if oldname in names or h > :
-        newname = a[names.index(oldname),1]
-        print(oldname, newname)
-        obj.write(head.replace(oldname, newname))
-    else:
-        obj.write(head)
+nid = []
+for h, head in enumerate(header):
+    newname = a[names.index(head),1]
+    if newname in nid:
+        print(newname)
+        print(header[nid.index(newname)], head)
+    nid.append(newname)
+
+obj = open(os.path.splitext(os.path.split(sys.argv[1])[1])[0]+'ENSG.tsv', 'w')
+obj.write('\t'+'\t'.join(np.array(nid))+'\n')
+for r in repfile[1:]:
+    obj.write(r)
+
 
